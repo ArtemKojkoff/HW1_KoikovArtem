4a3dd026c88a98fb52ccb5361075c22362287c33
git <artemkojkov@gmail.com>
2024-03-09 13:25:59 +0200

    Add project files.

---
+++ HW1_KoikovArtem/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HW1_KoikovArtem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User();
+            user1.Gender = Gend.Male;
+
+            Console.WriteLine("Enter firstname");
+            user1.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter lastname");
+            user1.LastName = Console.ReadLine();
+
+            Console.WriteLine(user1.ToString());
+
+            Console.ReadLine();        }
+    }
+}
---
+++ HW1_KoikovArtem/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1_KoikovArtem
+{
+    public enum Gend
+    {
+        Male,
+        Female,
+    }
+    internal class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; }
+        public Gend Gender {  get; set; }
+
+        public User() 
+        {
+            Age = 20;
+        }
+        public User(string firstName, string lastName, int age, Gend gender)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Gender = gender;
+        }
+
+        public override string ToString() 
+        {
+            return $"Hi, my name is {FirstName} and last name {LastName}. I am {Age} years old. I am {Gender}.";
+        }
+
+    }
+}
